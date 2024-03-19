612152a08068f27329cd0b3681cd7d147d725f74
mukkaragayathri23 <121806347+mukkaragayathri23@users.noreply.github.com>
2024-03-19 12:01:43 +0530

    Update AbstractonExample.java

--- abstract methods/Example.java
+++ abstract methods/Example.java
@@ -1,12 +1,45 @@
-package com.javaAbstractMethods;
+abstract class Shape {
+    public abstract double calculateArea();
+}
 
-public class Example extends Department
-{
+class Circle extends Shape {
+    private double radius;
 
-	@Override
-	public String getDetails() 
-	{
-		return "Example1 class details";
-	}
+    public Circle(double radius) {
+        this.radius = radius;
+    }
 
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+interface Drawable {
+    void draw();
+}
+
+class Rectangle implements Drawable {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
+
+public class AbstractionExample {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+
+        System.out.println("Area of circle: " + circle.calculateArea());
+        rectangle.draw();
+    }
 }
